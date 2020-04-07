60fbdbe62d57d81020889a400fc53b42ae81ba4c
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-07 17:25:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,10 @@ def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def introduction_with_language_optional(name,language="java")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
